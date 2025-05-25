6264a5c5d03b0f81edf8902f8c77610aa0dd6144
Patrick Seute <patrick.seute@cox.net>
2025-05-25 00:45:56 -0400

    remove redundant catch

--- src/twitchsync/__main__.py
+++ src/twitchsync/__main__.py
@@ -2,8 +2,4 @@ import sys
 from twitchsync import main_cli
 
 if __name__ == "__main__":
-    try:
-        main_cli()
-    except KeyboardInterrupt:
-        print("\nInterrupted by user, exiting...")
-        sys.exit(130)
+    main_cli()
